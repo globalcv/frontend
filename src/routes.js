ebb60e9532371a742ca47680ab273b4d8d5a13ec
kkm <kubasurdej@gmail.com>
2020-03-13 13:16:46 +0100

    Update routes to match new paths

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-import Home from './components/Home'
-import Contact from './components/Contact'
-import User from './components/User/User'
-import Register from './components/User/Register'
-import Login from './components/User/Login'
+import Home from "./views/Home";
+import Contact from "./views/Contact";
+import User from "./views/User/User";
+import Register from "./views/User/Register";
+import Login from "./views/User/Login";
 
 export const routes = [
-    { path: '*', redirect: "/" },
-    { name: 'home', path: '/', component: Home},
-    { name: 'contact', path: '/contact', component: Contact },
-    { name: 'register', path: '/register', component: Register },
-    { name: 'login', path: '/login', component: Login },
-    { name: 'user', path: '/user', component: User }
-]
+  { path: "*", redirect: "/" },
+  { name: "home", path: "/", component: Home },
+  { name: "contact", path: "/contact", component: Contact },
+  { name: "register", path: "/register", component: Register },
+  { name: "login", path: "/login", component: Login },
+  { name: "user", path: "/user", component: User }
+];
