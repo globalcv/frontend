5ce6aede3ad61a36363df79b2504ecc6397267fb
kkm <kubasurdej@gmail.com>
2020-03-13 13:34:56 +0100

    Convert main file to typescript

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+const API_URL = "http://localhost:8000";
+
+Vue.mixin({
+  data() {
+    return {
+      get apiUrl() {
+        return API_URL;
+      }
+    };
+  }
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
